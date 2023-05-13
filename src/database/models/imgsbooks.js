870dbe4fbdb907de083d905cd4ed61bb6a03fcc2
Antonioog <arkanamos1985@gmail.com>
2023-05-13 01:12:35 -0600

    Creamoslos modelos de Books e ImgsBooks con sus respectivas migraciones

---
+++ src/database/models/imgsbooks.js
@@ -0,0 +1,37 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ImgsBooks extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ImgsBooks.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      imgs_books_url: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      book_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'ImgsBooks',
+    }
+  );
+  return ImgsBooks;
+};
